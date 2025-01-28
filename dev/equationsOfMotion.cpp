646d4a55b8d281982f33d4f8f7b1df6312d20c7e
davidspiv <daspivack@gmail.com>
2025-01-27 19:45:47 -0800

    testing integrating equations of motion

---
+++ dev/equationsOfMotion.cpp
@@ -0,0 +1,36 @@
+#include "../include/io.h"
+#include "../include/struct.h"
+#include <iostream>
+#include <math.h>
+
+float getAcc(float pos)
+{
+   const float G = 6.674e-11; // gravitational constant
+   const float M = 1.9891e30; //[kg] mass of sun
+   const float m = 5.9722e24; //[kg] mass of earth
+   const float r = 1.4959e11; //[meters] distance between sun and earth
+
+   return G * (M + m) * pos / pow(r, 3);
+}
+
+int main()
+{
+   const Planet earth = {"earth",   1.00000011, 0.01671022, 0.00005,
+                         -11.26064, 102.94719,  357.51716,  365.259636};
+
+   Cord acc{0, 0, 0};
+   Cord vel{0, 29780, 0};
+   Cord pos{1.4959e11, 0, 0};
+
+   Cord velocities[365];
+   velocities[0] = vel;
+
+   for (size_t i = 0; i < 365; i++)
+   {
+      acc.x = getAcc(pos.x);
+      acc.y = getAcc(pos.y);
+      acc.z = getAcc(pos.z);
+   }
+
+   print(pos.x);
+}
